bbb0a3ddaccd791461fb10e95a15575885b23970
oyajikun <oyajikun>
2011-02-13 08:38:20 +0900

    added prepare_decrypted_token before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :prepare_decrypted_token
+
+  # = フィルタで暗号化トークンを復号化し、環境変数としてセットする
+  # Author:: oyajikun
+  def prepare_decrypted_token
+    request.env["decrypted_token"] = Crypt.to_hash(
+      Crypt.config[:password], Crypt.config[:salt], params[:encrypted_token], ['_', '='])
+  end
 end
