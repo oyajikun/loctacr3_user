30c3baa56d9c997f0565867cba55a078baae2947
oyajikun <oyajikun>
2011-03-06 06:21:19 +0900

    change session_store from cache to db

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Loctacr3User::Application.config.session_store :cookie_store, :key => '_loctacr3_user_session'
+#Loctacr3User::Application.config.session_store :cookie_store, :key => '_loctacr3_user_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Loctacr3User::Application.config.session_store :active_record_store
+Loctacr3User::Application.config.session_store :active_record_store
